71dfc134734e95e9704dcd1c6a5b21682f5ef0d0
Mroone1998 <773981994@qq.com>
2018-03-13 23:18:08 +0800

    move cpp to another document

---
+++ code/main.cpp
@@ -0,0 +1,10 @@
+#include "headers.h"
+
+int main()
+{
+	//calib(1,2);
+	string path = "F:\\Documents\\Graduation Project\\Matching\\Picture DataSet\\calibration picture\\";
+	int imageNum = 13;
+	cameraCalibrate(path,Size(6,9),13,14);
+	
+}
